625b4952b7bfd1a776645cff2b4564d2ee4216db
takayuki5689 <46898837+takayuki5689@users.noreply.github.com>
2019-02-03 20:04:38 +0900

    Delete StatusRepository.java

--- src/connectdb/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package connectdb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import connectdb.Status;
-
-@Repository
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
